2a52faf046cbcac3414b989b3eb88c781a953e6d
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2024-07-08 09:55:59 +0530

    Update cluster-autoscaler.tf

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -1,3 +1,4 @@
+/*
 module "eks-cluster-autoscaler" {
   source  = "lablabs/eks-cluster-autoscaler/aws"
   version = "2.2.0"
@@ -6,3 +7,4 @@ module "eks-cluster-autoscaler" {
   cluster_identity_oidc_issuer     = module.eks.oidc_provider
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
 }
+*/
