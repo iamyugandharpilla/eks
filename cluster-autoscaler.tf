29cc788089b6f97e92f1386981c1e0468df3ef78
iamyugandharpilla <160424692+iamyugandharpilla@users.noreply.github.com>
2024-07-30 14:05:44 +0530

    Update cluster-autoscaler.tf

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -1,4 +1,4 @@
-/*
+
 module "eks-cluster-autoscaler" {
   source  = "lablabs/eks-cluster-autoscaler/aws"
   version = "2.2.0"
@@ -7,5 +7,5 @@ module "eks-cluster-autoscaler" {
   cluster_identity_oidc_issuer     = module.eks.oidc_provider
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
 }
-*/
+
 
